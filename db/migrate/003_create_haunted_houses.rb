07ac3d41b198c81e404fed6b59d01715a672c8be
Carrie Savinell <carrielane89@gmail.com>
2018-07-03 14:28:07 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,15 +1,15 @@
 class CreateHauntedHouses < ActiveRecord::Migration
 
-def change
-  create_table :haunted_houses do |h|
-    h.string :name
-    h.string :location
-    h.string :theme
-    h.float :price
-    h.boolean :family_friendly
-    h.datetime :opening_date
-    h.datetime :closing_date
-    h.text :description
+  def change
+    create_table :haunted_houses do |h|
+      h.string :name
+      h.string :location
+      h.string :theme
+      h.float :price
+      h.boolean :family_friendly
+      h.datetime :opening_date
+      h.datetime :closing_date
+      h.text :description
+    end
   end
-end
 end
